ddea3d701b9158c6ce0dca6ea907b49ca02173e7
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-17 10:50:21 +0545

    1

---
+++ day3.py
@@ -0,0 +1,14 @@
+
+# Bizzare Food Recipe
+
+food = input("Name a food > ")
+fruit = input("Name a type of fruit > ")
+method = input("Name a method of cooking > ")
+add = input("Name a desert to add > ")
+diy = input("Name a DIY item > ")
+
+print ("MENU")
+print(method,food,"with", fruit, " and", add, " on a bed of",diy)
+
+
+
