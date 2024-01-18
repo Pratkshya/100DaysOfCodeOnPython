1d3b349b4a0f0afb72df3003e4cfd6203154664e
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-18 22:21:41 +0545

    IF THIS ELSE THAT

---
+++ CharacterSimulatorDay5.py
@@ -0,0 +1,50 @@
+print("""....So no one told you life was gonna be this way
+Your job's a joke, you're broke
+Your love life's DOA
+It's like you're always stuck in second gear
+When it hasn't been your day, your week, your month
+Or even your year, but
+I'll be there for you....
+""")
+print("F" + "\033[31m" + "." + "\033[0m" +
+  "R" + "\033[34m" + "." + "\033[0m" +
+  "I" + "\033[33m" + "." + "\033[0m" +
+  "E" + "\033[31m" + "." + "\033[0m" +
+  "N" + "\033[31m" + "." + "\033[0m" +
+  "D" + "\033[34m" + "." + "\033[0m" +
+  "S!!!""\n")
+print("Are you excited to find out which","F" + "\033[31m" + "." + "\033[0m" +
+     "R" + "\033[34m" + "." + "\033[0m" +
+     "I" + "\033[33m" + "." + "\033[0m" +
+     "E" + "\033[31m" + "." + "\033[0m" +
+     "N" + "\033[31m" + "." + "\033[0m" +
+     "D" + "\033[34m" + "." + "\033[0m" +
+     "S", "character you and your friends'd relate to in the real life?")
+print("Then, answer the following questions:" "\n")
+
+choose = input("Are you a control freak or a chill out person? > ")
+if choose == "control freak":
+  print("Gee. You're a MONICA. Do you have your Lobster yet? 🤭 ")
+
+else :
+  print ("WOW. You're a PHOEBE. Can you play Smelly Cat? 🙀","\n")
+
+option = input("Are you socially awakward or not? ? > ")
+if option == "yes":
+  print ("Hi, I make jokes when I'm uncomfortable..Yes, You're a CHANDLER. Do I have to be anymore clear?😜 \n")
+
+else :
+  print("I guess you like girls on bread. LOL. You're a big time JOEY.🤫", "\n")
+
+again = input ("Are you super duper clingy whilst in a relationship? > ")
+if again.lower() == "yes":
+  print("UNAGI☝️ 👉 👨. You're a ROSS, no doubt.")
+else :
+ print("Hmm, You're a RACHEL. Were you ever been on a break?🫣 ")
+
+
+
+
+
+
+
---
+++ day5.py
@@ -0,0 +1,9 @@
+myname = input("What is your name? > ")
+
+if myname == "Pratikshya":
+    print ("""Welcome Home!!. You are one of the biggest comedian 
+    I've ever met in my life.😂""")
+else:
+   print ("""Wait, why are you here? Run or we will hunt you 
+   down !!!😡😡😡""")
+  
