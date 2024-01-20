d7680b28353530adf3c0a6067ade14d13ece3b01
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-20 10:24:36 +0545

    7

---
+++ day7.1.py
@@ -0,0 +1,20 @@
+seriesname = input("Your go to binge watching series : ")
+if seriesname == "modern family":
+  print ("""The love for this series is enormous. How I wish to 
+  have a partner like Phill. Hehe.  """)
+
+elif seriesname == "the big bang theory":
+  print("Really?")
+  favcharacter = input("Who is your favourite male character : ")
+  choose = input("Penny or bernadette : ")
+ 
+  if favcharacter == "sheldon" and choose == "penny":
+    print("""A witty yet lovable character, paired up wit
+    Penny, whose intellect outsource sheldon's. They are like bread with butter. Inseparable.""")
+    print("My respect for you just went up.")
+    
+elif seriesname == "breaking bad":
+ print("""Best series ever made in the history. There I said it. 
+ NO COMMENTS""")
+else:
+  print("Hmm...Not a great taste to binge watch..I pass...")
