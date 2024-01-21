5d578e48d5381771d6c89191f2a4dc5400feea59
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-21 21:36:07 +0545

    affirmation_generator

---
+++ Projectday8.py
@@ -0,0 +1,31 @@
+print ("<--Affirmation Generator!-->")
+print("+++++++++++++++++++++++")
+
+name = input("Enter your name: ")
+day = input("Which day is today? : ")
+date = input("Enter the date: ")
+print ("Welcome " + name + "!")
+
+if day == "Sunday" or day =="sunday":
+  print("""First day after a weekend. I feel sorry for you to have to wakeup this early and getting ready for school. But remember this is your day, make it worth it✨!!!!""")
+
+elif day == "Monday" or day =="monday":
+  print ("""Always dress nice to have a good day. And lipstick is go to. Make people bark around you. Great day ahead🏵️!!""")
+
+elif day == "Tueday" or day =="tueday":
+  print("""Be yourself. Only you know you. So being content isn't always bad. Trust yourself😁!""")
+
+elif day == "Wednesday" or day =="wednesday":
+  print("""Sometimes it's okay to take a step back and give it a rest for a while. Even robot needs reboot and recharging. Take it easy😸!""")
+
+elif day == "Thurday" or day =="thurday":
+  print("""You are getting there. Don't give up. Take it slow but steady. Everybody has their day💪!""")
+
+elif day == "Friday" or day =="friday":
+  print("""You are on fire. Remember, it's okay to lie if you can have your way into it. Nobody is a saint. If they say they are, then boy you'll know.🤫""")
+
+elif day == "Saturday" or day =="saturday":
+  print("Saturday!! You deserve a break. Go out and have fun. Books can wait for few more hours.😉")
+
+else:
+  print("Try again😡!!!!!!!")
