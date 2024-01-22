dfe0fe4c8211c29404b096964644b04225c04ce2
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-22 21:38:08 +0545

    generation_generator

---
+++ Generation_Generator_9.3.py
@@ -0,0 +1,23 @@
+print(".....GENERATION GENERATOR.....")
+
+year = int(input("Which year were you born in? : "))
+
+if year >= 1925 and year <= 1946 :
+  print("You are the traditionalists! I assume, you value your ancient norms and culture, which is phenomenal.")
+
+elif year >= 1947 and year <= 1964 :
+  print ("""You're the Baby boomers. You must have had gone through a lot after the World War II along with technological transformation, change in social and traditional cultures, population growth. You have my blessings!""")
+
+elif year >= 1965 and year <= 1981 :
+  print("""You're the Generation X. The transitional age from the traditional forms of communication to the rise of internet. A really big change indeed!""")
+
+elif year >= 1982 and year <= 1995 :
+  print("""You're the Millenials. The ultimate generation with the upper hand in every possible thing. From internet to mobile phones in addition to many more.""")
+
+elif year >= 1996 and year <= 2015 :      
+ print("""You're the Gen Z a.k.a Generation Z. Nothing is left unexplored. Everybody is showered with great technical discoveries and what not.""")
+
+else:
+  print("""You're still flourishing as a Gen Z. You have all what you need, make no expections!""")
+
+        
