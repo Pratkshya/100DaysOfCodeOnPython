1b0c47fda09a1d7368618b3fe41bd77a93b2addd
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-22 20:38:09 +0545

    float

---
+++ day9.2.py
@@ -0,0 +1,9 @@
+#Use of Float.
+print ("Math IQ Test")
+value = float(input("Enter the value of PI --> "))
+if value == 3.142:
+  print("Correct! You're a genious🥳.")
+
+else :
+  print("Not a math geek I see😝.")
+  
