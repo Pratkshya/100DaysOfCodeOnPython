0e10b6a2eb27ae660a7d3d65d1d6f94dbde0b8b7
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-22 20:23:15 +0545

    int

---
+++ day9.1.py
@@ -0,0 +1,10 @@
+print("WIN OR LOSE....")
+score = int(input("Enter your score --> "))
+
+if score > 1000:
+  print("Congratulations !!!! You won the battle.🥳")
+
+else:
+  print("Better luck next time😝.")
+
+#int() is used to convert string into an integer so it compares the value with a integer.
