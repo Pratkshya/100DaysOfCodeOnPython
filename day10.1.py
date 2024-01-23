ca2260894151e6287e611bc2fc0f7dd62f41aac6
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-23 22:01:11 +0545

    basic_math

---
+++ day10.1.py
@@ -0,0 +1,26 @@
+#Basic mathematics
+
+adding = 7 + 3
+print(adding)
+
+subtraction = 8 - 9
+print(subtraction)
+
+multiplication = 4 * 11
+print(multiplication)
+
+division = 25 / 5
+print(division)
+
+# a number raised to the power of some number
+# in this example we raise 5 to the power of 2
+squared = 4**2
+print(squared)
+
+# remainder of a division
+modulo = 17 % 4
+print(modulo)
+
+# whole number division
+divisor = 9 // 2
+print(divisor)
