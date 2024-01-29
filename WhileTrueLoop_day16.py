c04a636053c05de3f6a8f436157080a69d83d782
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-01-29 22:04:29 +0545

    day_16

---
+++ WhileTrueLoop_day16.py
@@ -0,0 +1,9 @@
+print("Use of while loop!")
+print()
+while True :
+  question = input("Do you adore me (Y or N) ? : ") #Infinite loop untill the user inputs N(No).
+  
+  if question == "N": #If condition is false, it jumps into the next line due to break.
+   
+   break
+print("Your loss😡")    
