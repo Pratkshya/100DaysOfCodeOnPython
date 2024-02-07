3404deaceb161b354660767ddf9b79a232b48b9f
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-02-07 22:32:46 +0545

    debug

---
+++ Debug_day22.2.py
@@ -0,0 +1,16 @@
+import random
+
+for i in range (10):
+ MyNumber = random.randint(1,100)
+ print(MyNumber)
+
+
+ 
+
+ """ Before debugging
+ import random
+
+  MyNumber = random.randint(1,100)
+for i in range (10):
+ print(MyNumber) = prints the same value 10 times after a random number is choosen because of the condition used before the loop.
+ """
