ef1bc0f526c586bc918d4943f936bc1b00e069ae
Pratikshya Parajuli <147973972+Pratkshya@users.noreply.github.com>
2024-02-09 22:19:22 +0545

    use_of_parameters

---
+++ Parameters_day24.py
@@ -0,0 +1,9 @@
+def whichcake(ingrediants):
+    print("Here we go...")
+    if whichcake == "chocolate":
+        print("Hmm, chocolate cake does sound amazing!")
+    elif ingrediants == "chicken":
+        print("Are you mad or what?")
+    else:
+        print("We can work on that.")    
+whichcake("Buff")    
